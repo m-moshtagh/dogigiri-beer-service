752270e511bf2c624d4ccfd277abb55db29cacdf
Mohammad Moshtagh <m-moshtagh@pm.me>
2022-02-17 17:10:57 +0330

    project initialized! closes #1

---
+++ src/main/java/com/dogigiri/beerservice/DogigiriBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dogigiri.beerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogigiriBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogigiriBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dogigiri/beerservice/DogigiriBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogigiri.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogigiriBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
